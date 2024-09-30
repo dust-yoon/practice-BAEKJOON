e4eefd3c715863657b24b1f718e3fca1fffb9adc
JIYUN <popowjy12@konkuk.ac.kr>
2024-10-01 00:03:37 +0900

    백준 2869

--- 2869_240922.py
+++ 2869_240922.py
@@ -1,21 +1,22 @@
 # 2869 (달팽이는 올라가고 싶다)
 
-import math
 import sys
+import math
 input = sys.stdin.readline
 
-while True:
-    a, b, v = map(int, input().split())
+a, b, v = map(int, input().split())
 
-    m = v-a
-    n = a-b
+day = math.ceil((v - a) / (a - b)) + 1
 
-    if m % n == 0:
-        day = int(m / n + 1)
-    else:
-        day = ((v - (v % n)) // n) + 1
-        # (v // n) + 1
-    print(day)
+# m = v-a
+# n = a-b
+
+# if m % n == 0:
+#     day = m // n + 1
+# else:
+#     day = ( v // n) + 1
+
+print(day)
 
 ## import math
 # 올림 하는 방법 - math.ceil()
