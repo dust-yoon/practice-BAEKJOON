2cf0153509493eae7d29e2fa1b5fdcbdad508f18
JIYUN <popowjy12@konkuk.ac.kr>
2024-09-30 19:46:29 +0900

    백준 1436

--- 1436_240925.py
+++ 1436_240925.py
@@ -1,2 +1,15 @@
 # 1436 (영화감독 숌)
 
+N = int(input())
+time = 0
+number = 0
+
+while True:
+    number += 1
+
+    if '666' in str(number):
+        time += 1
+    
+        if time == N:
+            print(number)
+            break
