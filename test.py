a5181ad7a15240d9acf19d665049d66bab94c9e4
JIYUN <popowjy12@konkuk.ac.kr>
2024-10-07 10:43:31 +0900

    test

---
+++ test.py
@@ -0,0 +1,114 @@
+# k = ["2", "3", "5", "6", "7"]
+# print(k.index("2"))
+# m = [[]]*3
+# print(m)
+# m[0].append(3)
+# print(m)
+
+# l = [[]
+#     ,[]
+#     ,[]]
+# print(l)
+# l[0].append(3)
+# # print(l)
+
+# A = []
+# plus = list(map(int, input().split()))
+# A.append(plus)
+# print(A)
+
+# # 1 2 3
+# A = [[]]*3
+# B = [[],[],[]]
+# A[0].append(1)
+# B[0].append(1)
+# print(A)
+# print(B)
+
+# # 결과가 왜 다르게 나오나요
+# 참조 개념, 객체 개념 파악하기
+
+# c = []
+# c.append([])
+# print(c)
+
+# array = [[0] * 2 for _ in range(3)]
+# print(array)
+# array[0].append(1)
+# print(array)
+
+# print(*[a for a in range(1, 101)])
+
+# print(list('12345'))
+# word = 'qwertgfbvxdzsadfgb'
+# print(len(word))
+# print(word.find('p'))
+
+# print(word.count('e'))
+
+# for i in range(81):
+#         if 2**i < 95 < 2**(i+1):
+#             ans = i
+#             break
+# print(ans)
+
+# i = [1, 0, 1, 1]
+# i = map(str, i)
+# print(''.join(i))
+
+# rest = {}
+# for k in range(42):
+#     rest[k] = 0
+# print(rest)
+
+
+# 1해보고 없으면 2도 2도 없으면 없음
+# 걍 다 작은 수부터 시작해서 있으면 하고 없음 말고
+# n = 216
+# a + b + c + 100*a + 10*b + c = 216
+# a 1 (a는 0 아니면 1)
+# b c 10*b c = 115
+# b 11보다 작은 수 중에 가장 큰 수 = 9
+
+# 1123
+
+
+# a = 0 넣어보고 
+# b c 로 감당?
+# # import sys
+# # a, b, c = map(int, sys.stdin.readline().split())
+# # print(a)
+
+
+# total = 0
+# i = 216
+# for j in range(len(str(i))):
+#     total += int(i[j])
+#         if total == N:
+#             answer = i
+#             break
+# print(answer)
+
+# print(len('1'))
+# print(round(1234,3))
+
+# lst = [1, 2]*3
+# print(lst[5])
+# print(len('asdfg')//2)
+# i = 3
+# print(-i)
+# lst = []
+# for i in range(1,1):
+#     lst.append(i)
+# print(lst)
+# num = '121'
+# print(num[1])
+# print(num[-1])
+
+# lst = [1, [2, 3, 4], 5]
+# lst.flatten()
+# print(lst)
+
+name = 'dfsdfsdfsdf'
+
+print(name, end='\n')
