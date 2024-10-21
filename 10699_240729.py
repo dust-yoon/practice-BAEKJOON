81bc6c41e76372bb12a356cdd2981c6ea0c21231
JIYUN <popowjy12@konkuk.ac.kr>
2024-10-21 22:11:35 +0900

    백준

--- 10699_240729.py
+++ 10699_240729.py
@@ -6,7 +6,7 @@ print(time.strftime('%Y-%m-%d'))
 # <사고과정>
 
 # 1. UTC의 날짜 확인
-#     datetime.datetime.today()
+#     datetime.sdatetime.today()
 # 2. 9시간을 더해 서울의 날짜 설정하기
 #     datetime.timedelta(hours=9)
 # 3. time 출력
@@ -17,3 +17,17 @@ print("'-..-'|   ||   |")
 print("'-..-'|_.-;;-._|")
 print("'-..-'|   ||   |")
 print("'-..-'|_.-''-._|")
+
+print('  ___  ___  ___')
+print('  | |__| |__| |')
+print('  |           |')
+print('   \_________/')
+print('    \_______/')
+print('     |     |')
+print('     |     |')
+print('     |     |')
+print('     |     |')
+print('     |_____|')
+print('  __/       \__')
+print(' /             \\')
+print('/_______________\\')
