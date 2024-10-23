b7286507550b0bb547d66a3c2cc0afc66de9bdb7
JIYUN <popowjy12@konkuk.ac.kr>
2024-10-23 17:06:43 +0900

    백준 4714

---
+++ 4714_241023.py
@@ -0,0 +1,7 @@
+# 4714 (Lunacy)
+
+while True:
+    heavy = float(input())
+    if heavy < 0:
+        break
+    print(f'Objects weighing {heavy:.2f} on Earth will weigh {heavy*0.167:.2f} on the moon.')
