7864c5d31e4d8dcb5a0085af80e2606ecb542ed0
JIYUN <popowjy12@konkuk.ac.kr>
2024-11-07 00:46:25 +0900

    백준 2579

---
+++ 2579_241106.py
@@ -0,0 +1,38 @@
+# 2579 (계단 오르기)
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+for i in range(N):
+    new = []
+    new.append(int(input()))
+    new.append(i+1)
+    lst.append(new)
+
+total = lst[N-1][0]
+lst.pop()
+lst.sort(reverse=True)
+stack = set([N])
+
+for a in lst:
+    index = int(a[1])
+    number = int(a[0])
+    if index+1 in stack:
+        if not index-1 in stack and not index+2 in stack:
+            total += number
+            stack.add(index)
+        else:
+            continue
+    elif index-1 in stack:
+        if not index-2 in stack and not index+1 in stack:
+            total += number
+            stack.add(index)
+        else:
+            continue
+    else:
+        total += number
+        stack.add(index)
+
+print(total)
