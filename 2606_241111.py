137c23b902e86454aba4c7af4f65349df4f3f2c5
JIYUN <popowjy12@konkuk.ac.kr>
2024-11-12 23:45:52 +0900

    백준 2606

--- 2606_241111.py
+++ 2606_241111.py
@@ -1,55 +1,24 @@
 # 2606 (바이러스)
 
-# 7대
-# 네트워크 상 직접 연결되어있는 컴퓨터 쌍의 수
-# 6
-#
-#   1
-# 2 - 5
-# 3   6
-#
-# 4 - 7
-#
-# 딕셔너리
-# 1:2 2:3 1:5 이런식으로 연결
-# 개수 변수 생성
-# 있는지 없는지 확인
-# def 로 이어지게 해보자 재귀함수로
-# 있으면 삭제, 삭제했으면 한번 더 돌려보기(중복 방지)
-# 만들면서 += 실행하기
-
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-dic = {}
+dic = {i : [] for i in range(1, N+1)}
+
 for _ in range(M):
     a, b = map(int, input().split())
-    if a in dic:
-        dic[a].append(b)
-    else:
-        dic[a] = list(b)
+    dic[a].append(b)
+    dic[b].append(a)
 
-time = set([1])
+visit = set()
+
 def virus(N):
-    time.add(N)
-    if dic[N]:
-        return virus(dic[N].pop())
-    elif not dic[N]:
-        dic[N].pop()
+  visit.add(N)
+  for virus_friend in dic[N]:
+    if virus_friend not in visit:
+      virus(virus_friend)
 
-print(len(time))
-
-dic = {3:[1,2],5:3}
-dic[6] = [1]
-print(dic)
-dic[6].append(7)
-print(dic)
-dic[6].pop()
-print(dic)
-dic[6].pop()
-print(dic)
-if not dic[6]:
-  dic.pop(6)
-print(dic)
+virus(1)
+print(len(visit) - 1)
