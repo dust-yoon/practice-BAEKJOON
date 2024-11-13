370bf27b2e7c8e99633d457cec3b02973e8f447f
JIYUN <popowjy12@konkuk.ac.kr>
2024-11-14 00:32:13 +0900

    백준 9375

--- 9375_241112.py
+++ 9375_241112.py
@@ -1,1 +1,19 @@
 # 9375 (패션왕 신해빈)
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    dic = {}
+    for _ in range(N):
+        name, category = map(str, input().split())
+        if category in dic:
+            dic[category] += 1
+        else:
+            dic[category] = 1
+    case = 1
+    for i in dic:
+        case *= dic[i]+1
+    print(case-1)
