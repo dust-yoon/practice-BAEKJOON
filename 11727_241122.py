6b49092b49b8308197a1fe4ded63cf3411345601
JIYUN <popowjy12@konkuk.ac.kr>
2025-01-23 12:44:21 +0900

    백준 11727

--- 11727_241122.py
+++ 11727_241122.py
@@ -2,19 +2,20 @@
 
 import sys
 input = sys.stdin.readline
+
 N = int(input())
-number = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+number = [0, 1, 3, 5, 11, 21, 43, 85, 171]
 k = len(number)
-if N < len(number):
+if N < k:
     ans = number[N]
 else:
     number.extend([0]*(N-k+1))
     for i in range(N-k+1):
         i += k
-        number[i] = number[i-1] + number[i-2]
+        if i%2:
+            number[i] = number[i-1]*2 - 1
+        else:
+            number[i] = number[i-1]*2 + 1
     ans = number[N]
 
-
-
-
-print(ans%10007) 
+print(ans%10007)
