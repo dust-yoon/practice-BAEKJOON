ae1aeaf71ff91a544fc3adb2ba6b5e55a8555e4f
JiYunWon <popowjy12@konkuk.ac.kr>
2025-04-27 08:56:58 +0900

    백준 5524

---
+++ 5524_250225.py
@@ -0,0 +1,9 @@
+# 5524 (입실관리)
+# 32412 KB / 36 ms / 132 B
+
+import sys
+input = sys.stdin.readline
+N = int(input())
+for _ in range(N):
+    sentence = input().strip()
+    print(sentence.lower())
