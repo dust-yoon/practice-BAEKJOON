7fd933c9223c30d3fae0c3b045fe1c576a5c32b2
JiYunWon <popowjy12@konkuk.ac.kr>
2025-09-15 23:47:10 +0900

    백준 1012

--- 1012_250813.py
+++ 1012_250813.py
@@ -4,8 +4,23 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-
+# Devide test case
 for _ in range(T):
-    m,n,k = map(int, input().split())
-    # Make a check  board
+    # INPUT
+    M,N,K = map(int, input().split())
+    # Make a check board
+    board = []
+    for i in range(M):
+        board.append([])
+    # Fill board
+    for _ in range(K):
+        m,n = map(int, input().split())
+        board[m].append(n)
     
+    # OUTPUT
+    #비어있으면 삭제 후 새로운 애 데려와야해
+    for j in range(len(board)):
+        if board[j] == False:
+            board.remove(j)
+    
+        
